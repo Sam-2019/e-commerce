a5ccb50d67c1e79d6341fc49e7cd5945f7da09ad
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-05-10 18:41:42 +0000

    added order schema

---
+++ db/schema/order.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const OrderSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  products: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Product",
+    },
+  ],
+  timestamps: { createdAt: "created_at" },
+});
+
+module.exports = mongoose.model("Order", OrderSchema);
