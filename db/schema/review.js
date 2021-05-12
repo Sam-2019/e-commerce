a5fa0c9fc7666511457cd19c6c85ebc8f73b59a2
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-05-12 14:34:01 +0000

    created ReviewSchema

---
+++ db/schema/review.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ReviewSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  product: {
+    type: Schema.Types.ObjectId,
+    ref: "Product",
+  },
+  rating: {
+    type: Number,
+  },
+  text: {
+    type: String,
+  },
+});
+
+module.exports = mongoose.model("Review", ReviewSchema);
