cc3663fa66a40e996ebb4c97b9b7126b3cc1d1ea
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-05-25 14:20:11 +0000

    added timestamps

--- db/schema/review.js
+++ db/schema/review.js
@@ -1,21 +1,25 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const ReviewSchema = new Schema({
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
+const ReviewSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    product: {
+      type: Schema.Types.ObjectId,
+      ref: "Product",
+    },
+    rating: {
+      type: Number,
+    },
+    text: {
+      type: String,
+    },
   },
-  product: {
-    type: Schema.Types.ObjectId,
-    ref: "Product",
-  },
-  rating: {
-    type: Number,
-  },
-  text: {
-    type: String,
-  },
-});
+
+  { timestamps: { createdAt: "created_at", updatedAt: "updated_at" } }
+);
 
 module.exports = mongoose.model("Review", ReviewSchema);
