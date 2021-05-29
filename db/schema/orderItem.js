d18412838d0a72a5006d09741f8e006edecc1cab
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-05-29 18:44:31 +0000

    added payment field

--- db/schema/orderItem.js
+++ db/schema/orderItem.js
@@ -14,6 +14,10 @@ const OrderItemSchema = new Schema({
     type: Schema.Types.ObjectId,
     ref: "Product",
   },
+  payment: {
+    type: Schema.Types.ObjectId,
+    ref: "Payment",
+  },
   quantity: {
     type: String,
   },
