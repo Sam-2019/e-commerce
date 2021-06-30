2b86799af2103bde9489c8a94594f279d6746939
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-06-30 18:38:39 +0000

    fixed export error

--- helper.js
+++ helper.js
@@ -4,16 +4,18 @@ const jwt = require("jsonwebtoken");
 const saltRounds = 12;
 // const hashedPassword = await bcrypt.hash(password, saltRounds);
 
-export const passwordEncrypt = async (password) => {
+const passwordEncrypt = async (password) => {
   return await bcrypt.hash(password, saltRounds);
 };
 
-export const passwordCompare = async (password, savedPassword) => {
+const passwordCompare = async (password, savedPassword) => {
   return await bcrypt.compare(password, savedPassword);
 };
 
-export const generateToken = (userID, userEmail) => {
+const generateToken = (userID, userEmail) => {
   return jwt.sign({ ID: userID, email: userEmail }, "somesupersecretkey", {
     expiresIn: "1h",
   });
 };
+
+module.exports = { passwordEncrypt, passwordCompare, generateToken };
