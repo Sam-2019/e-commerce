9a841b9bcc3d059f3d21b1cfde583e36a7908144
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-07-05 18:23:55 +0000

    updated is-auth

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -1,14 +1,8 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-//  console.log(req.headers.authorization);
-
-  //console.log(req.get("Authorization"));
-
-  // console.log(req.headers.authorization);
-
   const authHeader = req.get("Authorization");
-  // console.log(authHeader);
+
   if (!authHeader) {
     req.isAuth = false;
     return next();
@@ -19,10 +13,9 @@ module.exports = (req, res, next) => {
     req.isAuth = false;
     return next();
   }
-  const secret = "somesupersecretkey";
   let decodeToken = null;
   try {
-    decodeToken = jwt.verify(token, secret);
+    decodeToken = jwt.verify(token, "somesupersecretkey");
   } catch (err) {
     req.isAuth = false;
     return next();
@@ -34,6 +27,6 @@ module.exports = (req, res, next) => {
   }
 
   req.isAuth = true;
-  req.userId = decodeToken.userId;
+  req.userID = decodeToken.userID;
   next();
 };
