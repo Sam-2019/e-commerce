e7a3603541cf12efeabe005af5168d1a075da39d
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-08-12 22:28:04 +0100

    Delete keys.js

--- db/keys.js
+++
@@ -1,6 +0,0 @@
-var api_key = "7STuj7naNvqQgZTu";
-var token = "samuel";
-
-module.exports = {
-  mongoURI: `mongodb+srv://${token}:${api_key}@cluster0.dottv.mongodb.net/e-commerce?retryWrites=true&w=majority`,
-};
